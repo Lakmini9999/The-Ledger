992f3da86c1707e3824708d9cd490b33188e0397
Lakmini9999 <manthrirathna.mal@gmail.com>
2018-11-05 07:18:09 +0530

    first com

---
+++ ims/Ims.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ims;
+
+/**
+ *
+ * @author Lucky
+ */
+public class Ims {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
